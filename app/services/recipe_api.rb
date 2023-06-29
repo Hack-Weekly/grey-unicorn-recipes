25b4921478570a2ba6761158a76a7c7fbafd47c6
Ian Withers <mr.bowtie989@gmail.com>
2023-06-29 16:16:30 -0400

    refactor: to use instance variables to track last query and offset for easier pagination of results

--- app/services/recipe_api.rb
+++ app/services/recipe_api.rb
@@ -1,11 +1,29 @@
 class RecipeApi
-  def self.request_recipes(user_query, recipe_count = 10)
+
+  attr_accessor :last_query, :offset
+
+  def initialize
+    @last_query = ''
+    @offset = 0
+  end
+
+  def request_recipes(user_query:, offset: 0)
+    # TODO: add error handling and handle no more results
+    @last_query = user_query
     conn = Faraday.new(
       url: 'https://api.api-ninjas.com',
-      params: { query: user_query, offset: recipe_count },
+      params: { query: user_query , offset: offset},
       headers: { "X-Api-Key": ENV['RECIPE_API_KEY'] }
-    )
+    ) { |f| f.response :json }
 
-    conn.get('/v1/recipe')
+    response = conn.get('/v1/recipe')
+
+    response.map { |recipe| convert_recipe(recipe) }
   end
+
+  def convert_recipe(json_body)
+    ApiRecipe.new(json_body)
+  end
+
+  # TODO: add pagintation logic
 end
