d350e6585570ced17df81b35fb90981d0ffc623b
stuart wilson <stuartwilson94@gmail.com>
2023-06-29 14:22:18 -0500

    fixed petergate but forgot to switch to a new branch first

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  ############################################################################################
+  ## PeterGate Roles                                                                        ##
+  ## The :user role is added by default and shouldn't be included in this list.             ##
+  ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
+  ## The multiple option can be set to true if you need users to have multiple roles.       ##
+  petergate(roles: [:admin, :editor], multiple: false)                                      ##
+  ############################################################################################ 
+ 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,6 +16,7 @@ class User < ApplicationRecord
   validates_presence_of :name
 
   has_many :comments, dependent: :destroy
+  has_many :recipes, dependent: :destroy
   mount_uploader :profile, ProfileUploader
   has_many :authorizations
   def first_name
