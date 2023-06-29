30d1aacd008cc06cbdfeb2c92785300535e5e8e3
stuart wilson <stuartwilson94@gmail.com>
2023-06-29 15:49:23 -0500

    added authorid to recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
   has_rich_text :body
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: "User", foreign_key: "author_id"
   has_many_attached :images, dependent: :destroy
 end
--- db/migrate/20230629193900_add_author_to_recipe.rb
+++ db/migrate/20230629193900_add_author_to_recipe.rb
@@ -1,5 +1,5 @@
 class AddAuthorToRecipe < ActiveRecord::Migration[7.0]
   def change
-    add_column :recipes, :author, :string
+    add_column :recipes, :author_id, :int
   end
 end
