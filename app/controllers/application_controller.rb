03a23a3f3ee894df5f505b37569f76bf2628825b
stuart wilson <stuartwilson94@gmail.com>
2023-06-29 13:31:13 -0500

    added gravatars, flash messages, some styling, devise login and authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,36 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :store_action
+  include CurrentUserConcern
+
+  def store_action
+    return unless request.get?
+    if (
+         request.path != "/users/sign_in" &&
+           request.path != "/users/sign_up" &&
+           request.path != "/users/password/new" &&
+           request.path != "/users/password/edit" &&
+           request.path != "/users/confirmation" &&
+           request.path != "/users/sign_out" && !request.xhr?
+       ) # don't store ajax calls
+      store_location_for(:user, recipes_path)
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_url
+  end
+
+  add_flash_types :info, :success, :warning
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: %i[name location employer age link profile],
+    )
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,64 @@
 module ApplicationHelper
+
+    def login_helper(style = "")
+      if !user_signed_in?
+        link_to("Register", new_user_registration_path, class: "navbar-item") +
+          " ".html_safe +
+          link_to("Login", new_user_session_path, class: "navbar-item")
+      else
+        link_to(
+          "Logout",
+          destroy_user_session_path,
+          data: {
+            turbo_method: :delete,
+          },
+          class: "navbar-item",
+        )
+      end
+    end
+
+  def active?(path)
+    "active" if current_page? path
+  end
+
+  def flash_class(level)
+    case level.to_sym
+    when :notice
+      'bg-primary'
+    when :success
+      'bg-success'
+    when :alert
+      'bg-warning'
+    when :error
+      'bg-danger'
+    else
+      'bg-info'
+    end
+  end
+
+  def authorized_to
+    logged_in?(:admin) || logged_in?(:author)
+  end
+
+  def profile_helper(user)
+    unless user.profile.file.nil?
+      image_tag(
+        member.profile.to_s,
+        width: 60,
+        height: 60,
+        style: "border-radius: 50%",
+      ).html_safe
+    else
+      gravatar_helper(user)
+    end
+  end
+
+  def gravatar_helper(user)
+    image_tag(
+      "https://gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email.downcase)}",
+      width: 60,
+      height: 60,
+      style: "border-radius: 50%;",
+    )
+  end
 end
