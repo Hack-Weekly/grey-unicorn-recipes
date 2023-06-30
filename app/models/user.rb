269529dbd1463af2fda1560a7b596f80372a52d8
stuart wilson <stuartwilson94@gmail.com>
2023-06-30 15:45:10 -0500

    fixed the nav, added login and logout links working on github oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github google_oauth2]
 
   validates_presence_of :name
 
