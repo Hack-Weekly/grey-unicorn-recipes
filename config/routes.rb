0e9c669b32ac4712b9fb55a853c2dc223d2ac434
Brandon Campbell <bmc1022@gmail.com>
2023-07-01 19:45:11 +1000

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
-
-  resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks",
+  }
 
-    devise_for :users, controllers: {
-      sessions: 'users/sessions',
-      registrations: "users/registrations",
-      omniauth_callbacks: "users/omniauth_callbacks",
-    }
+  root to: "recipes#index"
 
-  get root to: "recipes#index"
-
-  devise_scope :user do
-    get "users", to: "devise/sessions#new"
-  end
-  get "/recipes", to: "recipes#index"
+  resources :recipes, except: [:index]
 end
