493f5cb40de20ab4e2bdfaca2a25df061f2b3399
Brandon Campbell <bmc1022@gmail.com>
2023-07-08 00:42:46 +1000

    remove hello stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,5 @@
 
 import { application } from './application';
 
-import HelloController from './hello_controller';
-application.register('hello', HelloController);
-
 import AlertController from './alert_controller.js';
 application.register('alert', AlertController);
